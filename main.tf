e62a413c86fef0b1b2daba3354360a2e5124e8e7
fsadykov <sadykovfarkhod@gmail.com>
2023-05-24 23:20:18 -0500

    fix: Deleted comments from the external data file

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ data "external" "this" {
   program = ["bash", "-c", "./scripts/script.sh"]
 
   query = {
-    # arbitrary map from strings to strings, passed
-    # to the external program as the data query.
     id = "abc"
   }
 }
